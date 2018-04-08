146bb0d3cd8f52b139a638527682288f78485b1f
sktoiva <sampo.toiva@gmail.com>
2018-04-09 00:11:20 +0300

    Svg tracking of game

--- src/badtrainer/core.cljs
+++ src/badtrainer/core.cljs
@@ -9,13 +9,72 @@
 
 (defonce app-state (atom {:text "Hello world!"}))
 
+(def hits (atom []))
+(def current-hits (atom []))
 
-(rum/defc hello-world []
+(defn track-hits [event]
+  (let [x (.-clientX event)
+        y (.-clientY event)]
+    (swap! current-hits conj [x y])))
+
+(defn svg-coords [[x y]]
+  (let [svg (.getElementById js/document "field")
+        pt (.createSVGPoint svg)
+        _ (set! (.-x pt) x)
+        _ (set! (.-y pt) y)
+        svgPt (.matrixTransform pt (.. svg getScreenCTM inverse))]
+    [(.-x svgPt) (.-y svgPt)]))
+
+(defn draw-circle [coords]
+  (let [[x y] (svg-coords coords)]
+    [:circle {:cx x :cy y :r "2" :stroke "gray"}]))
+
+(defn draw-strokes [coords]
+  (when (seq coords)
+    (let [[x y] (-> coords first svg-coords)
+          path (concat ["M" x y]
+                       (->> coords
+                            rest
+                            (map svg-coords)
+                            (mapcat (fn [[x y]] ["L" x y]))))]
+      [:path {:d (clojure.string/join " " path) :fill "transparent" :stroke "gray"}])))
+
+(rum/defc field-comp
+  < rum/reactive
+  []
+  (let [coords (rum/react current-hits)]
+    ;; court measurements: width 610cm (+ 45 cm outside on both sides), height 1340 cm (+30 cm outside), scaled to half
+    [:svg {:id "field" :width "350px" :height "700px" :style {:border "1px solid black"}
+           :on-click #(track-hits %)}
+     (mapv draw-circle coords)
+     (draw-strokes coords)]))
+
+(rum/defc total-points < rum/reactive
+  []
+  (let [points (count (rum/react hits))]
+    [:div points]))
+
+(rum/defc end-button
+  []
+  [:button {:on-click (fn []
+                        (swap! hits conj @current-hits)
+                        (reset! current-hits []))}
+   "End sequence"])
+
+(rum/defc undo-button
+  []
+  [:button {:on-click (fn []
+                        (swap! current-hits pop))}
+   "Undo"])
+
+(rum/defc root []
   [:div
-   [:h1 (:text @app-state)]
-   [:h3 "Edit this and watch it change!"]])
+   (field-comp)
+   (end-button)
+   (undo-button)
+   (total-points)])
 
-(rum/mount (hello-world)
+(rum/mount (root)
            (. js/document (getElementById "app")))
 
 (defn on-js-reload []
